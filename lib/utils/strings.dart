f6ed47020368ce4b657530ef3105b9d3ac2f0189
dannestulla <dannestulla@gmail.com>
2022-08-11 11:05:20 -0300

    feat: added strings and extension functions to percentage selection

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -13,4 +13,12 @@ class Strings {
   static const home = "Home";
   static const carteiras = "Carteiras";
   static const movimentacoes = "Movimentações";
+  static const conversaoDeMoeda = "Conversão de moeda";
+  static const converterDe = "Converter de:";
+  static const converterMoeda = "Converter moeda";
+  static const cancelar = "cancelar";
+  static const confirmar = "Confirmar";
+  static const paraReceberEm = "Para receber em:";
+  static const montanteSerConvertido = "Montante a ser convertido";
+  static const montantePosConversao = "Montante pós conversao";
 }
